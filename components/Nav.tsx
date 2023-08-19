7776ed8dcca876bba1555967575860d0c6a05b2f
Stepan72 <st.nim@list.ru>
2023-08-19 08:33:31 +0600

    started Hero

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,34 @@
-function Nav() {
-  return <div>Nav</div>;
-}
+import { headerLogo } from "./../src/assets/images";
+import { hamburger } from "./../src/assets/icons";
+import { navLinks } from "../constants/index";
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full border-1 border-blue-100">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => {
+            return (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="font-montserrat leading-normal text-lg text-slate-gray"
+                >
+                  {item.label}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger_button" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Nav;
