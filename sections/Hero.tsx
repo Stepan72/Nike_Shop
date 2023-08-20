7f510df131a5e854001da83b3061470baf8e1a87
Stepan72 <st.nim@list.ru>
2023-08-20 06:10:39 +0600

    ShoeCard started

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,6 +1,64 @@
+import { Button, ShoeCard } from "../components";
+import { shoes, statistics } from "../constants";
+import { arrowRight } from "../src/assets/icons";
+import { bigShoe1 } from "../src/assets/images";
+
 function Hero() {
   return (
-    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section
+      className="xl:padding-1 wide:padding-r padding-b w-full border-2 border-red-500 flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+      id="home"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 ">
+            The New Arrival
+          </span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        </h1>
+        <p className="text-slate-gray font-montserrat text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="Shop Now" iconUrl={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => {
+            return (
+              <div key={stat.label}>
+                <p className="text-4xl font-palanquin font-bold">
+                  {stat.value}
+                </p>
+                <p className="leading-7 text-slate-gray font-montserrat">
+                  {stat.label}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoe1}
+          alt="shoe_collection"
+          width={610}
+          height={500}
+          className="object-contain z-10 relative"
+        />
+        <div>
+          {shoes.map((item) => {
+            return (
+              <div key={item.bigShoe}>
+                <ShoeCard />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 }
 
